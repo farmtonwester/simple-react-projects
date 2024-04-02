7f59ed6d5b6bc4fed9f95732a49a17aebf416293
Weston Farmer <wesfarmer11@gmail.com>
2024-04-02 14:53:46 -0500

    complete accordian component functionality

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -4,25 +4,35 @@ import data from "./data";
 import { useState } from "react"
 
 export default function Accordian() {
-
     const [selected, setSelected] = useState(null);
 
     function handleSingleSelection(currentId) {
         console.log(currentId);
+        setSelected(currentId === selected ? null : currentId);
     }
-
-    return <div className="wrapper">
+console.log(selected);
+    return (
+        <div className="wrapper">
             <div className="accordian">
                 {
                     data && data.length > 0 ?
-                    data.map(dataItem => <div className="item">
-                        <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
-                            <h3>{dataItem.question}</h3>
-                            <span>+</span>
-                        </div>
-                    </div> )
-                    : <div>No data found!</div>
+                    data.map(dataItem => 
+                        <div className="item">
+                            <div 
+                                onClick={() => handleSingleSelection(dataItem.id)} 
+                                className="title"
+                            >
+                                <h3>{dataItem.question}</h3>
+                                <span>+</span>
+                            </div>
+                            {
+                                selected === dataItem.id ? 
+                                <div className="content">{dataItem.answer}</div>
+                                : null
+                            }
+                        </div> 
+                    ) : <div>No data found!</div>
                 }
             </div>
-    </div>;
+    </div> );
 }
